4fe112d5c03cfcc28e2c956b127a4e36aef0a2b9
Michael <47297421+TacticalHorse@users.noreply.github.com>
2023-09-27 10:24:42 +0500

    Update Insert Many Rows.sql

--- Insert Many Rows.sql
+++ Insert Many Rows.sql
@@ -3,6 +3,4 @@ INSERT INTO "TableName" ("Column1","Column2","Column3")
     (1, 'SomeText1', 'AnotherText1')
     , (2, 'SomeText2', 'AnotherText2')
     , (3, 'SomeText3', 'AnotherText3')
-    , (4, 'SomeText4', 'AnotherText4')
-    , (5, 'SomeText5', 'AnotherText5')
 )AS tmp(col1,col2,col3);
